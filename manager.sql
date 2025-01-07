389957c0f5ca19bd33557063cb30174c28e4972e
MengyaoDu <mengyaodu992@gmail.com>
2025-01-07 23:13:43 +0800

    1.7Update manager.sql
    admin、notice、teacher、user

--- manager.sql
+++ manager.sql
@@ -20,7 +20,7 @@ CREATE TABLE `admin`  (
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES (1, 'admin', 'admin', '管理员', 'http://localhost:9090/files/1697438073596-avatar.png','ADMIN', '13677889922', 'admin@xm.com');
+INSERT INTO `admin` VALUES (1, 'admin', '123456', '管理员', 'http://localhost:9090/files/1697438073596-avatar.png','ADMIN', '13677889922', 'admin@xm.com');
 
 -- ----------------------------
 -- Table structure for notice
@@ -43,3 +43,52 @@ INSERT INTO `notice` VALUES (2, '所有功能都已完成，可以正常使用',
 INSERT INTO `notice` VALUES (3, '今天天气很不错，可以出去一起玩了', '今天天气很不错，可以出去一起玩了', '2023-09-05', 'admin');
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------
+-- Table structure for teacher
+-- ----------------------------
+CREATE TABLE `teacher` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '姓名',
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '角色',
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '头像',
+  `content` longtext COLLATE utf8mb4_unicode_ci COMMENT '介绍',
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '电话',
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '邮箱',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='老师信息表';
+
+-- ----------------------------
+-- Records of teacher
+-- ----------------------------
+INSERT INTO `teacher` VALUES (1, '张小橘子', '123456', '张小橘子', 'TEACHER', 'http://localhost:9090/files/1736260843136-8029cd8d921752f2759df04c123eaf90.png', '@张小橘子 #张小橘子#', '24321423', '3214123@gmail.com');
+INSERT INTO `teacher` VALUES (2, '梦梦', '123456', '梦梦', 'TEACHER', 'http://localhost:9090/files/1736261290804-a9507568f337cde4eeaaa886029aa310.png', '@HELLODANCE梦梦', '324235115', '1453415@gmail.com');
+INSERT INTO `teacher` VALUES (3, '金大铭', '123456', '金大铭', 'TEACHER', 'http://localhost:9090/files/1736261327301-c822aa006536035721fdf9378dc55ac2.png', '@HELLODANCE金大铭', '32144235', '343634634@gmail.com');
+INSERT INTO `teacher` VALUES (4, '噗噗', '123456', '噗噗', 'TEACHER', 'http://localhost:9090/files/1736261595161-09b48ad938f89ca6ba5dddefd6944441.png', '古希腊掌管控制的@噗pu', '13525426', '6346246@gmail.com');
+INSERT INTO `teacher` VALUES (5, '一开', '123456', '一开', 'TEACHER', 'http://localhost:9090/files/1736261634641-9266a92ae461742a3681e382a7eb2453.png', '@HELLODANCE一开\n', '7821687521', '3125215@gmail.com');
+INSERT INTO `teacher` VALUES (6, '书艺Sugi', '123456', '书艺', 'TEACHER', 'http://localhost:9090/files/1736261667790-18f27050f19d63dd1339ffc1ebe5b815.png', '飞行导师 @书艺Sugi-', '214124235', '54363463@gmail.com');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '姓名',
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '头像',
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '角色',
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '电话',
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '邮箱',
+  `account` double(10,2) DEFAULT '0.00' COMMENT '余额',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户信息表';
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'ddd', '123456', 'ddd', 'http://localhost:9090/files/1736151609820-f2d0bab56f4a505cbaeeae197516fe2f.jpg', 'USER', '13618010974', '2537426018@qq.com', 0.00);
+INSERT INTO `user` VALUES (2, 'aaa', '123456', 'aaa', 'http://localhost:9090/files/1736152228825-7aea42891d4c5e3866a0642630826272.jpg', 'USER', '12432542', '4234235@gmail.com', 0.00);
+INSERT INTO `user` VALUES (3, 'bbb', '123456', 'bbb', 'http://localhost:9090/files/1736152265320-19d1d9aa028ccede99da20506e10da60.jpg', 'USER', '52522352', '23524@qq.com', 0.00);
